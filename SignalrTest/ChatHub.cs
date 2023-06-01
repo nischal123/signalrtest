a5279f07e8b2b250e49bb0f0b6689f0fe6ef460e
Nischal Sharma <nischal@192-168-1-106.tpgi.com.au>
2023-06-02 03:16:27 +1000

    init

---
+++ SignalrTest/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalrTest
+{
+   
+        public class ChatHub : Hub
+        {
+            public async Task SendMessage(string user, string message)
+            {
+                await Clients.All.SendAsync("ReceiveMessage", user, message);
+            }
+        }
+    
+
+}
+
---
+++ SignalrTest/wwwroot/chat.js
@@ -0,0 +1,23 @@
+﻿
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/chathub")
+    .build();
+
+connection.on("ReceiveMessage", function (user, message) {
+    const encodedUser = $("<div />").text(user).html();
+    const encodedMessage = $("<div />").text(message).html();
+    $("#messagesList").append(`<li><strong>${encodedUser}:</strong> ${encodedMessage}</li>`);
+});
+
+connection.start().catch(function (err) {
+    console.error(err.toString());
+});
+
+$("#sendButton").click(function () {
+    const user = $("#username").val();
+    const message = $("#messageInput").val();
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        console.error(err.toString());
+    });
+    $("#messageInput").val("");
+});
